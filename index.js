e0437818e0b81ba424b6538e581176c3044dc1d3
rpdecks <rpdecks@gmail.com>
2020-05-19 20:30:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(ary, cb){
+    let newAry = []
+
+    for (let i = 0; i < ary.length; i++) {
+        newAry.push(cb(ary[i]))
+    }
+    return newAry
+}
+
+function reduce(ary, start=0, cb){
+    let agg = start;
+
+    for (let i = 0; i < ary.length; i++) {
+        agg  
+    }
+}
